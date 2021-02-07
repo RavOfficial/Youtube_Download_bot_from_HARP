8f864ec259ca44649287421a1a88c3b5da569150
Anjana Madushanka <anjanama2007@gmail.com>
2021-02-07 12:05:15 -0800

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -31,7 +31,7 @@ async def ytdl(_, message):
         await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
         return
     buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-    sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
+    sentm = await message.reply_text("Processing Youtube Url 🧐")
     try:
         # Todo add webp image support in thumbnail by default not supported by pyrogram
         # https://www.youtube.com/watch?v=lTTajzrSkCw
