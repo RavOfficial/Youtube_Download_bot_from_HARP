a1ddfe834d68681f86709f7590110f5606c6bf3f
Anjana Madushanka <anjanama2007@gmail.com>
2021-02-07 12:25:58 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("1681788639:AAGI5887wJRfqWD8HxPUJnlBD5btRtg1ySU")
+BOT_TOKEN = os.environ.get("1696407804:AAEWFXbnuA2x2YmWDaXazmUXvzpng3E4_h0")
 APP_ID = int(os.environ.get("2710778"))
 API_HASH = os.environ.get("a3e5503b94be442badc605ca7905c83c")
 
