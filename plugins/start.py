cd760206f58905868eab3cbf432e3b657df7d659
Bawwa Official <78470759+OfficialBawwa@users.noreply.github.com>
2021-03-23 22:41:09 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,8 +5,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Update Channel 🇱🇰", url="https://t.me/Harp_Tech")]
-        [InlineKeyboardButton("Update Chat 🇱🇰", url="https://t.me/HARP_Chat")]
+        [InlineKeyboardButton("HARP Tech 🇱🇰", url="https://t.me/Harp_Tech")]
     ])
     welcomed = f"<b>Hey {message.from_user.first_name} \nIf you need help 🙄 Just click /help\n\n</b>"
     await message.reply_text(welcomed, reply_markup=joinButton)
