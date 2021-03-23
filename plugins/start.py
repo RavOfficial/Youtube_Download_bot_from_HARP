a564ce0d466482d4d310a344e61d856d07e2cdff
Bawwa Official <78470759+OfficialBawwa@users.noreply.github.com>
2021-03-23 22:34:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Update Channel 🇱🇰", url="https://t.me/Harp_Tech")]
-        [InlineKeyboardButgon("Update Chat 🇱🇰", url="https://t.me/HARP_Chat")]
+        [InlineKeyboardButton("Update Chat 🇱🇰", url="https://t.me/HARP_Chat")]
     ])
     welcomed = f"<b>Hey {message.from_user.first_name} \nIf you need help 🙄 Just click /help\n\n</b>"
     await message.reply_text(welcomed, reply_markup=joinButton)
